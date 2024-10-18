354c3bf82951e03d2d27d866b26d27aad6ffd9b8
Park Gijun <97518829+keejuneman@users.noreply.github.com>
2024-10-18 13:44:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,11 @@ def crawl_wanted(years_min, years_max, selected_jobs):
     # Chrome 옵션 설정
     chrome_options = Options()
     chrome_options.add_argument("--start-maximized")
-
+    chrome_options.add_argument("--headless")  # GUI 없이 실행
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    
     # chrome_options.add_argument("--headless")
     # chrome_options.add_argument("--disable-gpu")
     # chrome_options.add_argument("--window-size=1920,1080")
